6a751c5681f4749b7213178ab62b2f9ae8544d84
Nishad Uzzaman <nishad@gmail.com>
2021-11-07 17:21:26 +0300

    Ommitted weekends

--- getstockprices.py
+++ getstockprices.py
@@ -1,8 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas
-from datetime import date
+import datetime
 import os
+
+weekno = datetime.datetime.today().weekday()
+
+is_weekday = False
+if weekno < 5:
+    is_weekend = True
+else:  # 5 Sat, 6 Sun
+    is_weekend = False
 
 
 def getData(stock_symbol):
@@ -14,7 +22,7 @@ def getData(stock_symbol):
     soup = BeautifulSoup(r.text, 'html.parser')
     # Open,High,Low,Close,Adj Close,Volume
     stock = {
-        'Date': [date.today()],
+        'Date': [datetime.date.today()],
         'Open': [soup.find('b', {'id': 'rtPrev'}).text],
         'High': [soup.find('b', {'id': 'rtHi'}).text],
         'Low': [soup.find('b', {'id': 'rtLo'}).text],
@@ -24,13 +32,14 @@ def getData(stock_symbol):
     return stock
 
 
-with open('symbols.csv') as f:
-    lines = f.read().splitlines()
-    for symbol in lines:
-        print(symbol)
-        data = pandas.DataFrame.from_dict(getData(symbol))
-        if os.path.isfile("datasets/{}.csv".format(symbol)):
-            data.to_csv("datasets/{}.csv".format(symbol), mode='a', header=False, index=False)
-        else:
-            data.to_csv("datasets/{}.csv".format(symbol), mode='a', header=True, index=False)
-        data.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+if is_weekday:
+    with open('symbols.csv') as f:
+        lines = f.read().splitlines()
+        for symbol in lines:
+            print(symbol)
+            data = pandas.DataFrame.from_dict(getData(symbol))
+            if os.path.isfile("datasets/{}.csv".format(symbol)):
+                data.to_csv("datasets/{}.csv".format(symbol), mode='a', header=False, index=False)
+            else:
+                data.to_csv("datasets/{}.csv".format(symbol), mode='a', header=True, index=False)
+            data.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
